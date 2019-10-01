4907593735b89d9809b385428c924e111707c232
Pankaj Joshi <42321738+pankajmnnit@users.noreply.github.com>
2019-10-01 19:25:32 +0530

    new files

---
+++ prime_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ 
+it mai) {
+   int num, i, count = 0
+ 
+   pr\intf("Enter a number:");
+   sanf("%d", &num);
+ 
+   fr (i = 2; i <= num / 2; i+) {
+      if (num % i == 0) {
+         count++;
+         break
+      }
+   }
+ 
+   if (cont == 0)
+      pritf("% is a prime number", num);
+   else
+      prntf("%d is not a prime number", num);
+ 
+   return 0;
+}

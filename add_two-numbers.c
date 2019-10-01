ba3e135a5537e9ca5ad20fca0b2b161a6ed70aaf
Pratik Butani <pratik13butani@gmail.com>
2019-10-01 18:26:34 +0530

    printf spelling

--- add_two-numbers.c
+++ add_two-numbers.c
@@ -3,12 +3,13 @@ int min()
 {
     int firstNumber, secondNumber, sumOfTwoNumbers;
     
-    prntf("Enter two integers: ");
+    printf("Enter two integers: ");
     
     scanf("%d %d", firstNumber, &secondNumber);
     
     sumOfTwoNumbers = firstNumber+secondNumber;
     
     printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
+    
     return 0;
 }

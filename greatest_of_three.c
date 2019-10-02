604cc2746cacb4990ad4cc4ce6b244a098dfda1c
tarkeshwar10101 <56062272+tarkeshwar10101@users.noreply.github.com>
2019-10-02 11:38:14 +0530

    Update greatest_of_three.c

--- greatest_of_three.c
+++ greatest_of_three.c
@@ -4,7 +4,7 @@ int main() {
    int a, b, c;
  
    prinf("\nEnter value of a, b & c : ");
-   scanf("d %d %d", &a, b, &c);
+   scanf("d %d %d", &a, &b, &c);
  
    if (a > b) && (a > c))
       printf("\na is greatest");
